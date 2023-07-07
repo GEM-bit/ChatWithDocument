ef2b9d7f665b40c76f9d52d94f1cf51890d3aa8b
GEM-bit <71544961+GEM-bit@users.noreply.github.com>
2023-07-07 06:39:55 +0200

    Updated to include question answer

--- ChatWithDocuments.py
+++ ChatWithDocuments.py
@@ -65,6 +65,11 @@ def calculate_embedding_cost(text):
     #print(f'Tokan cost in R: {total_tokens / 1000 * 0.0001 * 18,76 }')
     return total_tokens,(total_tokens / 1000 * 0.0001 * 18,76)
 
+def clear_history():
+    if 'history' in st.session_state:
+        del st.session_state['history']
+
+
 # #######################################################################################################################
 if __name__ == "__main__":
     import os
@@ -72,7 +77,7 @@ if __name__ == "__main__":
     load_dotenv(find_dotenv(),override=True)
 
     st.image('LogoBlue.png',width=50)
-    st.subheader('LLM Question Answering Application')
+    st.subheader('LLM Question Answering Application :information_desk_person:')
 
     with st.sidebar:
         api_key=st.text_input('OpenAI API Key: ',type='password')    #If user doesn't have access to dotenv
@@ -80,31 +85,40 @@ if __name__ == "__main__":
             os.environ['OPENAI_API_KEY'] = api_key
 
         uploaded_file=st.file_uploader('Upload a file',type=["pdf","txt","docx"])
-        chunk_size=st.number_input('Chunk size: ',min_value=100,max_value=2048,value=512)
-        k=st.number_input('k: ',min_value=1,max_value=20,value=3)
-        add_data=st.button('Add data')
+        chunk_size=st.number_input('Chunk size: ',min_value=100,max_value=2048,value=512,on_change=clear_history)
+        k=st.number_input('k: ',min_value=1,max_value=20,value=3,on_change=clear_history)
+        add_data=st.button('Add data',on_click=clear_history)
         
-if uploaded_file and add_data:
-    with st.spinner('Reading, chunking and embedding file...'):
-        bytes_data = uploaded_file.read()
-        file_name=os.path.join('./',uploaded_file.name)         #Storing in current directory
-        with open(file_name,'wb') as f:
-            f.write(bytes_data)
-        data=load_document(file_name)
+        if uploaded_file and add_data:
+            with st.spinner('Reading, chunking and embedding file...'):
+                bytes_data = uploaded_file.read()
+                file_name=os.path.join('./',uploaded_file.name)         #Storing in current directory
+                with open(file_name,'wb') as f:
+                    f.write(bytes_data)
+                data=load_document(file_name)
 
-        chunks=chunk_data(data,chunk_size=chunk_size)
-        st.write(f'Chunk size: {chunk_size}, Chunks: {len(chunks)}')
-        tokens, embedding_cost = calculate_embedding_cost(chunks)
-        st.write('Embeddings Cost: R{embedding_cost:.4f}')    #Display with 4 decimal points
+                chunks=chunk_data(data,chunk_size=chunk_size)
+                st.write(f'Chunk size: {chunk_size}, Chunks: {len(chunks)}')
+                tokens, embedding_cost = calculate_embedding_cost(chunks)
+                st.write('Embeddings Cost: R{embedding_cost:.4f}')    #Display with 4 decimal points
+                
+                vector_store = create_embeddings(chunks)
+                st.session_state.vs=vector_store
 
-        vector_store = create_embeddings(chunks)
-        st.session_state.vs=vector_store
+                st.success('File uploaded, chunked and embedded')
 
-        st.success('File uploaded, chunked and embedded')
+    q = st.text_input('Ask a question about the content of your file: ')
+    if q:
+        if 'vs' in st.session_state:
+            vector_store = st.session_state.vs
+            answer = ask_and_get_answer(vector_store,q,k=3)
+            st.text_area(label='LLM answer: ',value=answer)
 
+            st.divider
 
-
-        
-
-
-
+            if 'history' not in st.session_state:
+                st.session_state.history =''
+            value=(f'Q: {q} /nAnswer: (answer)')
+            st.session_state.history = f'{value} /n{"-" * 50} /n {st.session_state.history}'
+            h = st.session_state.history
+            st.text_area(label='Chat History',value=h, key="history", height=400)
